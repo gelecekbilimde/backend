9ed4624ff542d7b01dd9985af310202b8ea049ee
halil-ulu <halil.1995@gmail.com>
2023-06-01 00:59:25 +0300

    imp : Response standartlaştırılması

---
+++ src/main/java/org/gelecekbilimde/scienceplatform/common/Response.java
@@ -0,0 +1,30 @@
+package org.gelecekbilimde.scienceplatform.common;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Response {
+
+	public static ResponseEntity<ApiResponse> ok(HttpServletRequest request, Object response){
+
+		ApiResponse apiResponse = new ApiResponse(request,HttpStatus.OK,response);
+		return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+	}
+
+	public static ResponseEntity<ApiResponse> noContent(){
+		return ResponseEntity.noContent().build();
+	}
+
+	public static ResponseEntity<ApiResponse> created(HttpServletRequest request, Object response){
+
+		ApiResponse apiResponse = new ApiResponse(request,HttpStatus.CREATED,response);
+		return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+	}
+	public static ResponseEntity<ApiResponse> multiStatus(HttpServletRequest request, Object response){
+
+		ApiResponse apiResponse = new ApiResponse(request,HttpStatus.MULTI_STATUS,response);
+		return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+	}
+
+}
--- src/main/java/org/gelecekbilimde/scienceplatform/system/VersionController.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/system/VersionController.java
@@ -1,15 +1,22 @@
 package org.gelecekbilimde.scienceplatform.system;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.gelecekbilimde.scienceplatform.common.ApiResponse;
+import org.gelecekbilimde.scienceplatform.common.Response;
 import org.gelecekbilimde.scienceplatform.exception.ClientException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 class VersionController {
 	@RequestMapping("/version")
-	public ResponseEntity<String> version() {
+	public  ResponseEntity<ApiResponse> version(HttpServletRequest request) {
 		//throw new ClientException("Versiyon Alınamadı");
-		return ResponseEntity.ok("API version: 0.0.1");
+		return Response.ok(request,"API version: 0.0.1");
 	}
 }
