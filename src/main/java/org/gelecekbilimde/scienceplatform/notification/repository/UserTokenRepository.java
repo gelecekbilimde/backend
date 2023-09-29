0f81a808f1365275a85c7aaf42bdee62b864d7bb
Said Murat ÖZDEMİR <20210808006@ogr.akdeniz.edu.tr>
2023-09-30 01:44:39 +0300

    imp : yorum satırı silindi.

--- src/main/java/org/gelecekbilimde/scienceplatform/notification/repository/UserTokenRepository.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/notification/repository/UserTokenRepository.java
@@ -7,7 +7,4 @@ import java.util.List;
 public interface UserTokenRepository extends JpaRepository<NotificationToken, Long> {
 
 	List<NotificationToken> findAllByUserId(Long userId);
-
-//	@Query("DELETE FROM NotificationToken u WHERE u.deviceToken IN ?1")
-//	void deleteDevicesById(List<String> tokens);
 }
