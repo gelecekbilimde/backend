1bdaf4f5e38db0f6e8131313e7567ecdca3445d8
Said Murat ÖZDEMİR <20210808006@ogr.akdeniz.edu.tr>
2023-10-02 07:53:49 +0300

    imp : Kod silinip yerine annotation eklendi.
    imp : Kod silinip yerine annotation eklendi. Firebase dependency versiyonu için properties kullanıldı.

--- src/main/java/org/gelecekbilimde/scienceplatform/notification/model/PushNotificationResponse.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/notification/model/PushNotificationResponse.java
@@ -1,10 +1,12 @@
 package org.gelecekbilimde.scienceplatform.notification.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class PushNotificationResponse {
 	private Status status;
 	private String message;
@@ -13,29 +15,5 @@ public class PushNotificationResponse {
 		SUCCESS,
 		FAILURE,
 		PENDING
-	}
-
-	public PushNotificationResponse(Builder builder) {
-		this.status = builder.status;
-		this.message = builder.message;
-	}
-
-	public static class Builder {
-		private Status status;
-		private String message;
-
-		public Builder status(Status status) {
-			this.status = status;
-			return this;
-		}
-
-		public Builder message(String message) {
-			this.message = message;
-			return this;
-		}
-
-		public PushNotificationResponse build() {
-			return new PushNotificationResponse(this);
-		}
 	}
 }
