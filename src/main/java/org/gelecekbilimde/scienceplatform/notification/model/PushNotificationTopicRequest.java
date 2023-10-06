d2cccef14b1c84fd3f9ff82a255ee122f07af532
Said Murat ÖZDEMİR <20210808006@ogr.akdeniz.edu.tr>
2023-10-06 18:02:40 +0300

    imp : unutulan Builder annotation eklendi.

--- src/main/java/org/gelecekbilimde/scienceplatform/notification/model/PushNotificationTopicRequest.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/notification/model/PushNotificationTopicRequest.java
@@ -1,43 +1,14 @@
 package org.gelecekbilimde.scienceplatform.notification.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class PushNotificationTopicRequest {
 	private String title;
 	private String message;
 	private String topic;
-
-	public PushNotificationTopicRequest(Builder builder) {
-		this.title = builder.title;
-		this.message = builder.message;
-		this.topic = builder.topic;
-	}
-
-	public static class Builder {
-		private String title;
-		private String message;
-		private String topic;
-
-		public Builder title(String title) {
-			this.title = title;
-			return this;
-		}
-
-		public Builder message(String message) {
-			this.message = message;
-			return this;
-		}
-
-		public Builder topic(String topic) {
-			this.topic = topic;
-			return this;
-		}
-
-		public PushNotificationTopicRequest build() {
-			return new PushNotificationTopicRequest(this);
-		}
-	}
 }
