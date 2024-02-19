38ed5cc56b423cdd426fc87bbd2bd826d0311936
Agit Rubar Demir <demiragitrubar@gmail.com>
2024-02-19 22:58:44 +0300

    refactor : `confirmation_token` Table Has Been Renamed as `user_verification` and Table Has Been Refactored

--- src/main/resources/db/migration/V5__seeds.sql
+++ src/main/resources/db/migration/V5__seeds.sql
@@ -1,8 +1,10 @@
-create table if not exists "confirmation_token"
+create table if not exists user_verification
 (
-  id             varchar(36)       not null primary key,
-  confirmation_token   varchar(255),
-  created_date     timestamp,
-  user_id         varchar(36),
-  CONSTRAINT fk_confirmation_token_user_user_id FOREIGN KEY (user_id) REFERENCES "user" (id)
-  );
+  id             varchar(36) not null primary key,
+  user_id        varchar(36) not null
+    constraint fk__user_verification__user_id references user (id),
+  create_user_id varchar(36) not null,
+  update_user_id varchar(36),
+  created_at     timestamp   not null,
+  updated_at     timestamp
+);
