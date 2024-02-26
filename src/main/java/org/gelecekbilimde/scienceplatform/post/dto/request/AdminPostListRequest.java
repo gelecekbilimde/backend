1f2a8275ba895f696cf87b5750b9c013b5543704
Agit Rubar Demir <demiragitrubar@gmail.com>
2024-02-26 18:38:19 +0300

    refactor : Unused `;` Character Has Been Removed

--- src/main/java/org/gelecekbilimde/scienceplatform/post/dto/request/AdminPostListRequest.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/dto/request/AdminPostListRequest.java
@@ -1,9 +1,8 @@
 package org.gelecekbilimde.scienceplatform.post.dto.request;
-;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.gelecekbilimde.scienceplatform.common.PagingRequest;
-
 
 @EqualsAndHashCode(callSuper = true)
 @Data
