6b9547d1448e6e745edfd98bb6b5532fa4e230cb
Agit Rubar Demir <demiragitrubar@gmail.com>
2024-03-02 13:24:59 +0300

    refactor : `likedAt` Field Has Been Removed

--- src/main/java/org/gelecekbilimde/scienceplatform/post/dto/domain/PostLikeDomain.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/dto/domain/PostLikeDomain.java
@@ -4,14 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @Builder
 public class PostLikeDomain {
 
 	private Integer likeCount;
-	private LocalDateTime likedAt;
 
 }
