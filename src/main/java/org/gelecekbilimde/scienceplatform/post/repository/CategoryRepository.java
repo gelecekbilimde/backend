93f2f258fce21a9fefe768cf4526feca817d9627
Said Murat ÖZDEMİR <114745121+SaidMuratOzdemir@users.noreply.github.com>
2024-04-15 11:01:38 +0300

    refactor: argument name changed.

--- src/main/java/org/gelecekbilimde/scienceplatform/post/repository/CategoryRepository.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/repository/CategoryRepository.java
@@ -14,5 +14,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	boolean existsByName(String name);
 
-	boolean existsByParentId(Long id);
+	boolean existsByParentId(Long parentId);
 }
