5e4ca2ea1ae18da351c8f8dac684a9d885d7fac1
agitrubard <demiragitrubar@gmail.com>
2024-04-18 15:24:04 +0300

    fix : Mapping of Category Create Request to Category Have Been Fixed

--- src/main/java/org/gelecekbilimde/scienceplatform/post/mapper/PostCreateRequestToPostModelMapper.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/mapper/PostCreateRequestToPostModelMapper.java
@@ -4,6 +4,7 @@ package org.gelecekbilimde.scienceplatform.post.mapper;
 import org.gelecekbilimde.scienceplatform.common.BaseMapper;
 import org.gelecekbilimde.scienceplatform.common.Util;
 import org.gelecekbilimde.scienceplatform.post.dto.request.PostCreateRequest;
+import org.gelecekbilimde.scienceplatform.post.model.Category;
 import org.gelecekbilimde.scienceplatform.post.model.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -12,12 +13,18 @@ import org.mapstruct.factory.Mappers;
 public interface PostCreateRequestToPostModelMapper extends BaseMapper<PostCreateRequest, Post> {
 
 
-	default Post mapForSaving(PostCreateRequest postCreateRequest, String userId){
+	default Post mapForSaving(PostCreateRequest postCreateRequest, String userId) {
 		return Post.builder()
 			.id(Util.generateUUID())
 			.header(postCreateRequest.getHeader())
 			.content(postCreateRequest.getContent())
-			.category(postCreateRequest.getCategory())
+			.category(Category.builder()
+				.name(postCreateRequest.getCategory().getName())
+				.order(postCreateRequest.getCategory().getOrder())
+				.slug(postCreateRequest.getCategory().getSlug())
+				.icon(postCreateRequest.getCategory().getIcon())
+				.parentId(postCreateRequest.getCategory().getParentId())
+				.build())
 			.userId(userId)
 			.likeCount(postCreateRequest.getLikeCount())
 			.active(postCreateRequest.isActive())
