3c19d848c01eeb5dec579239cb77658195cf2ec8
agitrubard <demiragitrubar@gmail.com>
2024-04-18 16:38:19 +0300

    refactor : `Degree` Enum Class Has Been Refactored

--- src/main/java/org/gelecekbilimde/scienceplatform/user/enums/Degree.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/user/enums/Degree.java
@@ -1,16 +1,12 @@
 package org.gelecekbilimde.scienceplatform.user.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Degree {
-	PRIMARY_SCHOOL("Primary School"),
-	MIDDLE_SCHOOL("Middle School"),
-	ASSOCIATE("Associate"),
-	BACHELOR("Bachelor"),
-	MASTER("Master"),
-	DOCTORATE("Doctorate");
-	private final String value;
+
+	PRIMARY_SCHOOL,
+	MIDDLE_SCHOOL,
+	ASSOCIATE,
+	BACHELOR,
+	MASTER,
+	DOCTORATE
+
 }
