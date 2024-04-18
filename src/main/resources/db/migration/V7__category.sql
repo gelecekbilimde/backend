97865c9c6dc253d8891eedfc5452b869600d2b73
agitrubard <demiragitrubar@gmail.com>
2024-04-18 09:39:14 +0300

    fix : DML Scripts Have Been Fixed

--- src/main/resources/db/migration/V7__category.sql
+++ src/main/resources/db/migration/V7__category.sql
@@ -24,23 +24,23 @@ alter table posts_process
 alter table posts_process
   add constraint fk_posts_process_category_id foreign key (category_id) references category;
 
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (0, 1, 'Fizik', 'fizik', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (1, 1, 'Biyoloji', 'biyoloji', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (2, 1, 'Kimya', 'kimya', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (1, null, 'Teknoloji', 'teknoloji', 'cpu', null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (2, null, 'Felsefe', 'felsefe', 'book', null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (1, 6, 'Ontoloji', 'ontoloji', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (2, 6, 'Ahlak Felsefesi', 'ahlak-felsefesi', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (0, 6, 'Epistemoloji', 'epistelomoji', null, null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (5, null, 'testCat', 'test-category', 'g', null, null, CURRENT_TIMESTAMP, null);
-INSERT INTO category (order_number, parent_id, name, slug, icon, create_user_id, update_user_id, created_at, updated_at)
-VALUES (0, null, 'Bilim', 'bilim', 'flask-conical', null, null, CURRENT_TIMESTAMP, null);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (0, 1, 'Fizik', 'fizik', null, CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (1, 1, 'Biyoloji', 'biyoloji', null, CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (2, 1, 'Kimya', 'kimya', null, CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (1, null, 'Teknoloji', 'teknoloji', 'cpu', CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (2, null, 'Felsefe', 'felsefe', 'book', CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (1, 6, 'Ontoloji', 'ontoloji', null, CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (2, 6, 'Ahlak Felsefesi', 'ahlak-felsefesi', CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (0, 6, 'Epistemoloji', 'epistelomoji', null, CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (5, null, 'testCat', 'test-category', 'g', CURRENT_TIMESTAMP);
+insert into category (order_number, parent_id, name, slug, icon, create_user_id, created_at)
+values (0, null, 'Bilim', 'bilim', 'flask-conical', CURRENT_TIMESTAMP);
