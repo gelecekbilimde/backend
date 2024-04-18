0cb8ea5510fb174c7acc4a27f3de49d0c403151b
agitrubard <demiragitrubar@gmail.com>
2024-04-18 09:45:54 +0300

    refactor : Unused Methods Have Been Removed from PostRepository

--- src/main/java/org/gelecekbilimde/scienceplatform/post/repository/PostRepository.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/repository/PostRepository.java
@@ -4,16 +4,8 @@ import org.gelecekbilimde.scienceplatform.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-
-import java.util.List;
-import java.util.Optional;
-
 public interface PostRepository extends JpaRepository<Post, Integer>, JpaSpecificationExecutor<Post> {
 
 	Post getById(String postId);
-
-	Optional<Post> getByIdAndLastProcess(String postId, Process processEnum);
-
-	boolean existsById(String postId);
 
 }
