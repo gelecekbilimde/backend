a30911cdd071e480df6f5575d07da5367832d17d
agitrubard <demiragitrubar@gmail.com>
2024-08-14 21:50:15 +0300

    refactor : `AuthKeyPairUtil` Has Been Refactored

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/util/AuthKeyPairUtil.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/util/AuthKeyPairUtil.java
@@ -2,56 +2,60 @@ package org.gelecekbilimde.scienceplatform.auth.util;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.gelecekbilimde.scienceplatform.common.exception.ServerException;
-import org.springframework.beans.factory.annotation.Value;
+import org.gelecekbilimde.scienceplatform.common.util.FileUtil;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.StringReader;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
-
+@Slf4j
 @UtilityClass
 public class AuthKeyPairUtil {
-	private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-		generator.initialize(2048);
-		return generator.generateKeyPair();
-	}
 
-	public static String getContentOfPrivateKey(String privateKeyPath) {
+	public static KeyPair generateKeyPair() {
 		try {
-			if(!Files.exists(Path.of(privateKeyPath))){
-				KeyPair pair = AuthKeyPairUtil.generateKeyPair();
-				return pair.getPrivate().toString();
-			}
-			byte[] keyBytes = Files.readAllBytes(Paths.get(privateKeyPath));
-			String keyContent = new String(keyBytes);
-			keyContent = keyContent.replaceAll("\\s+|-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----", "");
-			return keyContent;
-		} catch (Exception e) {
-			throw new ServerException("Private Key read error: " + e.getMessage());
-		}
-
-	}
-
-	public static String getContentOfPublicKey(String publicKeyPath) {
-		try {
-			if(!Files.exists(Path.of(publicKeyPath))){
-				KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-				generator.initialize(2048);
-				KeyPair pair = generator.generateKeyPair();
-				return pair.getPrivate().toString();
-			}
-			byte[] keyBytes = Files.readAllBytes(Paths.get(publicKeyPath));
-			String keyContent = new String(keyBytes);
-			keyContent = keyContent.replaceAll("\\s+|-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----", "");
-			return keyContent;
-		} catch (Exception e) {
-			throw new ServerException("Public Key read error: " + e.getMessage());
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (NoSuchAlgorithmException exception) {
+			log.error("KeyPair generation error: {}", exception.getMessage());
+			throw new ServerException(exception.getMessage());
 		}
 	}
+
+	public static PrivateKey findAndConvertPrivateKey(String path) {
+		String formattedPrivateKeyPem = FileUtil.findContent(path);
+		final StringReader keyReader = new StringReader(formattedPrivateKeyPem);
+		try {
+			PrivateKeyInfo privateKeyInfo = PrivateKeyInfo
+				.getInstance(new PEMParser(keyReader).readObject());
+			return new JcaPEMKeyConverter().getPrivateKey(privateKeyInfo);
+		} catch (IOException exception) {
+			log.error("PrivateKey conversion error: {}", exception.getMessage());
+			throw new ServerException(exception.getMessage());
+		}
+	}
+
+	public static PublicKey findAndConvertPublicKey(String path) {
+		String formattedPublicKeyPem = FileUtil.findContent(path);
+		StringReader keyReader = new StringReader(formattedPublicKeyPem);
+		try {
+			SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo
+				.getInstance(new PEMParser(keyReader).readObject());
+			return new JcaPEMKeyConverter().getPublicKey(publicKeyInfo);
+		} catch (IOException exception) {
+			log.error("PublicKey conversion error: {}", exception.getMessage());
+			throw new ServerException(exception.getMessage());
+		}
+	}
+
 }
