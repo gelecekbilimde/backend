a0d45b697ecafd6b0615edb89b6a354667dcfb56
Alp Emre Elmas <elmasalpemre@gmail.com>
2024-08-20 19:54:46 +0200

    imp: SQL has been added and refactored "UserFollowRepository"

--- src/main/java/org/gelecekbilimde/scienceplatform/user/repository/UserFollowRepository.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/user/repository/UserFollowRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserFollowRepository extends JpaRepository<UserFollowEntity, String> {
+public interface UserFollowRepository extends JpaRepository<UserFollowEntity, Long> {
 
 	Optional<UserFollowEntity> findByFollowedUserIdAndFollowerUserId(String followedUserId, String followerUserId);
 
--- src/main/resources/db/migration/V8__addIdColumnToUserFollowersTable.sql
+++ src/main/resources/db/migration/V8__addIdColumnToUserFollowersTable.sql
@@ -1,12 +1,5 @@
 ALTER TABLE user_followers
-  ADD COLUMN id SERIAL;
-
-UPDATE user_followers
-SET id = nextval('user_followers_id_seq')
-WHERE id IS NULL;
+  DROP CONSTRAINT IF EXISTS user_followers_pkey;
 
 ALTER TABLE user_followers
-  DROP CONSTRAINT IF EXISTS user_followers_pk;
-
-ALTER TABLE user_followers
-  ADD PRIMARY KEY (id, follower_user_id, followed_user_id);
+  ADD COLUMN id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
