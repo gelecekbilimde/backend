24dcb7c571765aa4a5dffbe1e2f233e648fd12c4
MUHAMMED EMIN KECIK <m.eminkecik@gmail.com>
2024-09-05 21:56:01 +0300

    feat: Request Modulü | Author ve Admin rolüne geçiş için gerekli endpointlerin oluşturulması #121

--- src/main/java/org/gelecekbilimde/scienceplatform/user/service/RoleService.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/user/service/RoleService.java
@@ -1,10 +1,19 @@
 package org.gelecekbilimde.scienceplatform.user.service;
 
-import org.gelecekbilimde.scienceplatform.user.model.request.UserToAuthorRequest;
+import org.gelecekbilimde.scienceplatform.common.model.response.Response;
+import org.gelecekbilimde.scienceplatform.user.model.request.RoleChangeRequest;
 import org.gelecekbilimde.scienceplatform.user.model.response.UserRoleResponse;
+
+import java.util.List;
 
 public interface RoleService {
 	UserRoleResponse userRoletoAuthorRoleRequest();
 
-	UserRoleResponse makeUserToAuthor(UserToAuthorRequest request);
+	UserRoleResponse makeUserToAuthor(RoleChangeRequest request);
+
+	List<UserRoleResponse> getAllUserRoletoAuthorRoleRequest();
+
+	Response deleteUserRoletoAuthorRoleRequest(RoleChangeRequest request);
+
+	UserRoleResponse makeUserToAdmin(RoleChangeRequest request);
 }
