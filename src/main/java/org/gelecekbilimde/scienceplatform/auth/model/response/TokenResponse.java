bd45522853b9cf0c23f134c70126eed979456045
agitrubard <demiragitrubar@gmail.com>
2024-09-06 16:31:47 +0300

    #126 | refactor: `@AllArgsConstructor` and `@NoArgsConstructor` Annotations Have Been Deleted from `TokenResponse`

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/model/response/TokenResponse.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/model/response/TokenResponse.java
@@ -1,16 +1,12 @@
 package org.gelecekbilimde.scienceplatform.auth.model.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class TokenResponse {
 
 	private String accessToken;
