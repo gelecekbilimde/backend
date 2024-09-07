11a783eed3218445c0ab70790d3361e0c90cf424
agitrubard <demiragitrubar@gmail.com>
2024-09-07 13:14:53 +0300

    #126 | refactor: `findPermissionsByRoleId()` Method Has Been Deleted Because It's Unused

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/repository/RoleRepository.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/repository/RoleRepository.java
@@ -1,18 +1,11 @@
 package org.gelecekbilimde.scienceplatform.auth.repository;
 
-import org.gelecekbilimde.scienceplatform.auth.model.entity.PermissionEntity;
 import org.gelecekbilimde.scienceplatform.auth.model.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, String> {
-
-	@Query("SELECT r.permissionEntities FROM RoleEntity r WHERE r.id = :roleId")
-	List<PermissionEntity> findPermissionsByRoleId(@Param("roleId") String roleId);
 
 	Optional<RoleEntity> getByIsDefaultTrue();
 
