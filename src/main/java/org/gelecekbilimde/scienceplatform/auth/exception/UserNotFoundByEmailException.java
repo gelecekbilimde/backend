5c1cfbcb39a24a45b3ff49c250b5dc7542fedf8d
agitrubard <demiragitrubar@gmail.com>
2024-09-16 17:38:50 +0300

    #19 | refactor: No Args Constructor of `UserNotFoundByEmailException` Class Has Been Removed Because It's Unused

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/UserNotFoundByEmailException.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/UserNotFoundByEmailException.java
@@ -7,11 +7,7 @@ import java.io.Serial;
 public final class UserNotFoundByEmailException extends NotFoundException {
 
 	@Serial
-	private static final long serialVersionUID = 3275707458511858867L;
-
-	public UserNotFoundByEmailException() {
-		super("user does not found!");
-	}
+	private static final long serialVersionUID = -6144535232442524262L;
 
 	public UserNotFoundByEmailException(String email) {
 		super("user does not found! email: " + email);
