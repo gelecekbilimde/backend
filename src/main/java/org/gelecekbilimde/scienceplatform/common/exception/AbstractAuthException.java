3a2f1a41ad89d590b5aef8ace089ef2ef348e4cb
agitrubard <demiragitrubar@gmail.com>
2024-09-16 17:19:24 +0300

    #19 | refactor: Constructor with `message` Parameter Has Been Added to `AuthException` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/common/exception/AbstractAuthException.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/common/exception/AbstractAuthException.java
@@ -5,6 +5,10 @@ import java.io.Serial;
 public abstract class AbstractAuthException extends RuntimeException {
 
 	@Serial
-	private static final long serialVersionUID = 2520784508718456462L;
+	private static final long serialVersionUID = 5834113326065055615L;
+
+	protected AbstractAuthException(String message) {
+		super(message);
+	}
 
 }
