cd3601dd6cb54e05e839e455e1af398c0ec8a896
agitrubard <demiragitrubar@gmail.com>
2024-09-16 17:31:45 +0300

    #19 | refactor: `UserNotFoundByEmailException` Class Has Been Refactored

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/UserNotFoundByEmailException.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/UserNotFoundByEmailException.java
@@ -7,14 +7,14 @@ import java.io.Serial;
 public final class UserNotFoundByEmailException extends UsernameNotFoundException {
 
 	@Serial
-	private static final long serialVersionUID = -6354173425539337989L;
+	private static final long serialVersionUID = 3275707458511858867L;
 
 	public UserNotFoundByEmailException() {
-		super("User not found!");
+		super("user does not found!");
 	}
 
 	public UserNotFoundByEmailException(String email) {
-		super("User not found! " + email);
+		super("user does not found! email: " + email);
 	}
 
 }
