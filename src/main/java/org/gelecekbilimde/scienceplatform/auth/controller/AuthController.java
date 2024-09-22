85424988055c386e3841b08efea3c44cfd985f4f
agitrubard <demiragitrubar@gmail.com>
2024-09-22 17:16:32 +0300

    #121 | refactor: Exception Handling Changes Have Been Integrated

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/controller/AuthController.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/controller/AuthController.java
@@ -3,16 +3,26 @@ package org.gelecekbilimde.scienceplatform.auth.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.gelecekbilimde.scienceplatform.auth.model.Token;
-import org.gelecekbilimde.scienceplatform.auth.model.request.*;
+import org.gelecekbilimde.scienceplatform.auth.model.request.LoginRequest;
+import org.gelecekbilimde.scienceplatform.auth.model.request.LogoutRequest;
+import org.gelecekbilimde.scienceplatform.auth.model.request.RefreshRequest;
+import org.gelecekbilimde.scienceplatform.auth.model.request.RegisterRequest;
+import org.gelecekbilimde.scienceplatform.auth.model.request.RoleChangeRequest;
+import org.gelecekbilimde.scienceplatform.auth.model.request.VerifyRequest;
 import org.gelecekbilimde.scienceplatform.auth.model.response.TokenResponse;
+import org.gelecekbilimde.scienceplatform.auth.model.response.UserRoleResponse;
 import org.gelecekbilimde.scienceplatform.auth.service.AuthenticationService;
 import org.gelecekbilimde.scienceplatform.auth.service.RegistrationService;
-import org.gelecekbilimde.scienceplatform.common.model.response.SuccessResponse;
-import org.gelecekbilimde.scienceplatform.auth.model.request.RoleChangeRequest;
-import org.gelecekbilimde.scienceplatform.auth.model.response.UserRoleResponse;
 import org.gelecekbilimde.scienceplatform.auth.service.RoleService;
+import org.gelecekbilimde.scienceplatform.common.model.response.SuccessResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -68,32 +78,32 @@ class AuthController {
 
 	@PutMapping("/requestToBeAuthor")
 	@PreAuthorize("hasRole('USER')")
-	public Response<UserRoleResponse> requestToBeAuthor() {
-		return Response.ok(roleService.userRoletoAuthorRoleRequest());
+	public SuccessResponse<UserRoleResponse> requestToBeAuthor() {
+		return SuccessResponse.success(roleService.userRoletoAuthorRoleRequest());
 	}
 
 	@PutMapping("/requestToBeAdmin")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Response<UserRoleResponse> requestToBeAdmin(@RequestBody @Valid RoleChangeRequest request) {
-		return Response.ok(roleService.makeUserToAdmin(request));
+	public SuccessResponse<UserRoleResponse> requestToBeAdmin(@RequestBody @Valid RoleChangeRequest request) {
+		return SuccessResponse.success(roleService.makeUserToAdmin(request));
 	}
 
 	@PutMapping("/makeUserAuthor")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Response<UserRoleResponse> makeUserAuthor(@RequestBody @Valid RoleChangeRequest request) {
-		return Response.ok(roleService.makeUserToAuthor(request));
+	public SuccessResponse<UserRoleResponse> makeUserAuthor(@RequestBody @Valid RoleChangeRequest request) {
+		return SuccessResponse.success(roleService.makeUserToAuthor(request));
 	}
 
 	@GetMapping("/getRequestToBeAuthor")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Response<List<UserRoleResponse>> getAllRequestToBeAuthor() {
-		return Response.ok(roleService.getAllUserRoletoAuthorRoleRequest());
+	public SuccessResponse<List<UserRoleResponse>> getAllRequestToBeAuthor() {
+		return SuccessResponse.success(roleService.getAllUserRoletoAuthorRoleRequest());
 	}
 
 	@DeleteMapping("/deleteRequestToBeAuthor")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Response deleteRequestToBeAuthor(@RequestBody @Valid RoleChangeRequest request) {
-		return Response.ok(roleService.deleteUserRoletoAuthorRoleRequest(request));
+	public SuccessResponse deleteRequestToBeAuthor(@RequestBody @Valid RoleChangeRequest request) {
+		return SuccessResponse.success(roleService.deleteUserRoletoAuthorRoleRequest(request));
 	}
 
 }
