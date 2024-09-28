1ac58aa2ac2d8f358599baf3fa443371c0b3ab93
Alp Emre Elmas <62440706+alpemreelmas@users.noreply.github.com>
2024-09-28 09:58:29 +0200

    feat(MockResultMatchersBuilders): json result matcher added to find path of general result keys

---
+++ src/test/java/org/gelecekbilimde/scienceplatform/util/MockResultMatchersBuilders.java
@@ -0,0 +1,67 @@
+package org.gelecekbilimde.scienceplatform.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
+
+@UtilityClass
+public class MockResultMatchersBuilders {
+
+	public static StatusResultMatchers status() {
+		return MockMvcResultMatchers.status();
+	}
+
+	public static JsonPathResultMatchers time() {
+		return MockMvcResultMatchers.jsonPath("$.time");
+	}
+
+	public static JsonPathResultMatchers header() {
+		return MockMvcResultMatchers.jsonPath("$.header");
+	}
+
+	public static JsonPathResultMatchers isSuccess() {
+		return MockMvcResultMatchers.jsonPath("$.isSuccess");
+	}
+
+	public static JsonPathResultMatchers response() {
+		return MockMvcResultMatchers.jsonPath("$.response");
+	}
+
+	public static JsonPathResultMatchers response(String path) {
+		return MockMvcResultMatchers.jsonPath("$.response.".concat(path));
+	}
+
+	public static JsonPathResultMatchers response(int index, String path) {
+		return MockMvcResultMatchers.jsonPath("$.response[" + index + "].".concat(path));
+	}
+
+	public static JsonPathResultMatchers responseSize() {
+		return MockMvcResultMatchers.jsonPath("$.response.size()");
+	}
+
+	public static JsonPathResultMatchers responses(String path) {
+		return MockMvcResultMatchers.jsonPath("$.response[*].".concat(path));
+	}
+
+	public static JsonPathResultMatchers content() {
+		return MockMvcResultMatchers.jsonPath("$.response.content");
+	}
+
+	public static JsonPathResultMatchers contents(String path) {
+		return MockMvcResultMatchers.jsonPath("$.response.content[*].".concat(path));
+	}
+
+	public static JsonPathResultMatchers contentSize() {
+		return MockMvcResultMatchers.jsonPath("$.response.content.size()");
+	}
+
+	public static JsonPathResultMatchers firstContent(String path) {
+		return MockMvcResultMatchers.jsonPath("$.response.content[0].".concat(path));
+	}
+
+	public static JsonPathResultMatchers subErrors() {
+		return MockMvcResultMatchers.jsonPath("$.subErrors");
+	}
+
+}
