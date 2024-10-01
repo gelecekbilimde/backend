c7c8c1f017fce21459b3617c352f5b649567f442
agitrubard <demiragitrubar@gmail.com>
2024-10-01 16:39:42 +0300

    #121 | refactor: `@Data` Annotation Has Been Removed from `RoleApplicationEntity` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/model/entity/RoleApplicationEntity.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/model/entity/RoleApplicationEntity.java
@@ -11,14 +11,16 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.gelecekbilimde.scienceplatform.auth.model.enums.RoleChangeStatus;
 import org.gelecekbilimde.scienceplatform.common.model.entity.BaseEntity;
 import org.gelecekbilimde.scienceplatform.user.model.entity.UserEntity;
 
-@Data
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
