5ee4f950fdec9e88117dc25eacede404d3a9d63f
agitrubard <demiragitrubar@gmail.com>
2024-10-02 20:17:45 +0300

    #121 | refactor: `fk__gb_role_application__role_id` Foreign Key Constraint Has Been Added to `gb_role_application` Table

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -312,5 +312,6 @@ create table if not exists gb_role_application
   updated_by varchar(255),
   updated_at timestamp(0),
   constraint fk__gb_role_changes__user_id foreign key (user_id) references gb_user (id),
+  constraint fk__gb_role_application__role_id foreign key (role_id) references gb_role (id),
   constraint c__gb_role_changes__status check ( status in ('IN_REVIEW', 'APPROVED', 'REJECTED'))
 );
