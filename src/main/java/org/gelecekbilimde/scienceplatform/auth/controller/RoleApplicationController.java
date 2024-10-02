c13fa0aec152b6c3679e94106253cc78d66c338f
agitrubard <demiragitrubar@gmail.com>
2024-10-02 20:04:43 +0300

    #121 | refactor: `@PostMapping` Annotation Has Been Used Instead of `@PutMapping` in Create Moderator Application Endpoint

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/controller/RoleApplicationController.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/controller/RoleApplicationController.java
@@ -14,7 +14,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,7 +55,7 @@ class RoleApplicationController {
 		return SuccessResponse.success();
 	}
 
-	@PutMapping("/role-application/moderator")
+	@PostMapping("/role-application/moderator")
 	@PreAuthorize("hasAnyAuthority('role:application:create:moderator')")
 	SuccessResponse<Void> createModeratorApplication() {
 
