7837f99dcaa127ef3d7386bb4b723476ca2e1a78
agitrubard <demiragitrubar@gmail.com>
2024-10-05 11:08:13 +0300

    #121 | refactor: `@Builder` Annotation Has Been Removed from `RoleSelfApplicationsResponse` Class Because It's Unused

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/model/response/RoleSelfApplicationsResponse.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/model/response/RoleSelfApplicationsResponse.java
@@ -1,13 +1,11 @@
 package org.gelecekbilimde.scienceplatform.auth.model.response;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.gelecekbilimde.scienceplatform.auth.model.enums.RoleApplicationStatus;
 
 @Getter
 @Setter
-@Builder
 public class RoleSelfApplicationsResponse {
 
 	private Role role;
