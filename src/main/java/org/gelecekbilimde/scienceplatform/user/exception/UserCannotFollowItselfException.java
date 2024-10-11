7927029f226ae813014cb8aa15f4b09a822dfb53
agitrubard <demiragitrubar@gmail.com>
2024-10-11 22:59:37 +0300

    GBS-44 | refactor: `final` Keyword Has Been Added to `UserCannotFollowItselfException` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/user/exception/UserCannotFollowItselfException.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/user/exception/UserCannotFollowItselfException.java
@@ -4,10 +4,10 @@ import org.gelecekbilimde.scienceplatform.common.exception.AbstractConflictExcep
 
 import java.io.Serial;
 
-public class UserCannotFollowItselfException extends AbstractConflictException {
+public final class UserCannotFollowItselfException extends AbstractConflictException {
 
 	@Serial
-	private static final long serialVersionUID = 8300066406018173223L;
+	private static final long serialVersionUID = -1651758308998005837L;
 
 	public UserCannotFollowItselfException(String userId) {
 		super("user cannot follow itself by userId:" + userId);
