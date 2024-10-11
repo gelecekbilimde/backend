d7d73e5487069731ca2de18ac3f2e19c017feb09
agitrubard <demiragitrubar@gmail.com>
2024-10-11 13:53:54 +0300

    GBS-44 | refactor: `@SuperBuilder` Annotation Has Been Removed from `TicketCommentResponse` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/ticket/model/response/TicketCommentResponse.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/ticket/model/response/TicketCommentResponse.java
@@ -2,13 +2,11 @@ package org.gelecekbilimde.scienceplatform.ticket.model.response;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@SuperBuilder
 public class TicketCommentResponse {
 
 	private Long id;
