a2ed2e0eaaa64ccfc2f6b40eb37c2473eb5f5340
agitrubard <demiragitrubar@gmail.com>
2024-10-11 23:03:06 +0300

    GBS-44 | refactor: `final` Keyword Has Been Added to `RoleNotFoundByNameException` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/RoleNotFoundByNameException.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/auth/exception/RoleNotFoundByNameException.java
@@ -2,10 +2,10 @@ package org.gelecekbilimde.scienceplatform.auth.exception;
 
 import java.io.Serial;
 
-public class RoleNotFoundByNameException extends RuntimeException {
+public final class RoleNotFoundByNameException extends RuntimeException {
 
 	@Serial
-	private static final long serialVersionUID = -927147689852209182L;
+	private static final long serialVersionUID = -4700198439829934412L;
 
 	public RoleNotFoundByNameException(String name) {
 		super("role not found with name: " + name);
