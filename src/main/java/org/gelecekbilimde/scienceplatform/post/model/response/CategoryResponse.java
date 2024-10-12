502c149eadb466395633c29697ae764d37b00341
agitrubard <demiragitrubar@gmail.com>
2024-10-12 23:13:36 +0300

    GBS-23 | `createdBy`, `createdAt`, `updatedBy`, `updatedAt` Fields Have Been Added to `CategoryResponse` Class

--- src/main/java/org/gelecekbilimde/scienceplatform/post/model/response/CategoryResponse.java
+++ src/main/java/org/gelecekbilimde/scienceplatform/post/model/response/CategoryResponse.java
@@ -4,6 +4,8 @@ package org.gelecekbilimde.scienceplatform.post.model.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class CategoryResponse {
@@ -14,4 +16,8 @@ public class CategoryResponse {
 	private String slug;
 	private String icon;
 	private Long parentId;
+	private String createdBy;
+	private LocalDateTime createdAt;
+	private String updatedBy;
+	private LocalDateTime updatedAt;
 }
